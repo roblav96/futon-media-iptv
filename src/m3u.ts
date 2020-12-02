6001f3bf8eb903db1a3519cf12c993fafedfd407
Robert Laverty <roblav96@gmail.com>
2020-12-02 08:47:49 -0500

    [Darwin] M  src/m3u.ts

--- src/m3u.ts
+++ src/m3u.ts
@@ -1,29 +1,22 @@
-import * as fs from 'https://deno.land/std/fs/mod.ts'
 import * as http from 'https://deno.land/std/http/mod.ts'
-import * as io from 'https://deno.land/std/io/mod.ts'
-import * as path from 'https://deno.land/std/path/mod.ts'
-import * as scanner from 'https://deno.land/x/scanner/mod.ts'
 
 export async function get(request: http.ServerRequest) {
-
-	let response = await fetch(Deno.env.get('M3U_URL') as string)
-	console.log('scanner ->', scanner)
-	console.log('response ->', response)
+	request.headers.delete('connection')
+	let response = await fetch(Deno.env.get('M3U_URL') as string, {
+		headers: request.headers,
+	})
 	let lines = (await response.text()).split('\n')
 	for (let i = 0; i < lines.length; i++) {
 		let line = lines[i]
 		if (line.includes('LATINO: ')) {
-			console.log('line ->', line)
 			lines.splice(i, 2)
 			i = i - 2
 		}
 	}
-	// console.log('lines ->', lines)
+	response.headers.delete('content-length')
 	return {
 		body: lines.join('\n'),
 		headers: response.headers,
 		status: response.status,
-	}
-	// console.log('text ->', text)
-	// return text
+	} as http.Response
 }
--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,9 @@
+import { intercept } from 'https://deno.land/x/function_intercept/index.ts'
+console.log = intercept(console.log).before((...data) => {
+	data[0] = `\n${data[0]}`
+	return data
+})
+
 import * as http from 'https://deno.land/std/http/mod.ts'
 import * as m3u from './m3u.ts'
 
@@ -9,12 +15,7 @@ for (let envkey of ['EPG_URL', 'M3U_URL']) {
 
 const server = http.serve({ hostname: '127.0.0.1', port: 8080 })
 for await (let request of server) {
-	console.log('request ->', request)
 	if (request.url == '/get_m3u') {
-		let response = await m3u.get(request)
-		response.headers.delete('content-length')
-		console.log('response.headers ->', response.headers)
-		request.respond(response)
-		// request.respond(await m3u.get())
+		request.respond(await m3u.get(request))
 	}
 }
