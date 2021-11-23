63ba63b7a2bb9f95bb55a2e4f964fe126f678846
Robert Laverty <roblav96@gmail.com>
2021-11-23 18:47:28 -0500

    [Darwin] M  src/m3u.ts

--- src/m3u.ts
+++ src/m3u.ts
@@ -27,9 +27,9 @@ export async function get(request: Request) {
 	let lskey = lskeys.find((v) => parseInt(v) > Date.now())!
 	let text = localStorage.getItem(lskey)
 	if (!text) {
-		console.time('fetch(M3U_URL)')
+		console.time(`fetch(${M3U_URL})`)
 		text = await (await fetch(M3U_URL)).text()
-		console.timeEnd('fetch(M3U_URL)')
+		console.timeEnd(`fetch(${M3U_URL})`)
 		localStorage.clear()
 		localStorage.setItem(`${Date.now() + new Date(0).setUTCMinutes(60)}`, text)
 	}
